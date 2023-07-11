bb2bbd239869c3ef8f2f1e724fb788d2a2ab340c
Georgia <georgia.m.vrana@gmail.com>
2023-07-11 13:33:53 -0400

    uploading for someone to review my error

---
+++ user.js
@@ -0,0 +1,9 @@
+async function main() {
+    const id = localStorage.getItem("id", id);
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`);
+    const postsData = posts.json();
+
+    console.log(postsData);
+}
+
+main()
