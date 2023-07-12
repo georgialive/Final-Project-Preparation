61f6fef15eb369b5be025a777906c4894c0f0cf0
Georgia <georgia.m.vrana@gmail.com>
2023-07-12 15:57:52 -0400

    cleaning up repeating code

--- user.js
+++ user.js
@@ -1,37 +1,29 @@
 const postListEL = document.querySelector(`.post-list`);
+const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
-    const id = event.target.value;
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`);
-    const postsData = await posts.json();
-    postListEL.innterHTML = postsData.map(post => `
-        <div class="post">
-        <div class="post__title">
-            ${post.title}
-        </div>
-        <p class="post__body">
-            ${post.body}
-        </p>
-        </div>
-    `).join('');
+    const id =event.target.value;
+    renderPosts(id)
 }
 
-async function main() {
-    const id = localStorage.getItem("id");
+async function renderPosts(id) {
+   
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`);
     const postsData = await posts.json();
-    console.log(postsData);
-
-    postListEL.innterHTML = postsData.map(post => `
-        <div class="post">
-        <div class="post__title">
-            ${post.title}
-        </div>
-        <p class="post__body">
-            ${post.body}
-        </p>
-        </div>
-    `).join('');
+    postListEL.innterHTML = postsData.map(post => postHTML(post)).join('');
 }
 
-main()
+function postHTML(post) {
+    return  `
+    <div class="post">
+    <div class="post__title">
+        ${post.title}
+    </div>
+    <p class="post__body">
+        ${post.body}
+    </p>
+    </div>
+`
+}
+
+renderPosts(id)
